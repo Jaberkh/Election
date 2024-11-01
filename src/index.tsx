de26a5850f981beaa86fcac878624c74aa47ddea
Jaberkh <khodadadi.jaber@live.com>
2024-11-01 10:12:12 +0330

    Updated code with new changes

--- src/index.tsx
+++ src/index.tsx
@@ -76,7 +76,7 @@ app.frame('/', (c) => {
   const harrisPercent = totalVotes ? Math.round((votes.harris / totalVotes) * 100) : 0;
   const trumpPercent = totalVotes ? Math.round((votes.trump / totalVotes) * 100) : 0;
 
-  const frameUrl = 'https://election-u-s.onrender.com';
+  const frameUrl = 'https://election-u-s.onrender.com/';
   const composeCastUrl = `https://warpcast.com/~/compose?text=I%20voted%20for%20${encodeURIComponent(
     selectedCandidate
   )}%2C%20what’s%20your%20opinion%3F%0A%0AFrame%20By%20@Jeyloo%0A${encodeURIComponent(frameUrl)}`;
